af67e2f3bd2b25da9c7cb68b6d1983d085b5d8b5
Kali Deming <kalideming@icloud.com>
2023-02-04 11:20:30 -0700

    altered destroy command order

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 puts "Clearing Database..."
-User.destroy_all
 ProjectRole.destroy_all
-Project.destroy_all
 Task.destroy_all
-Schedule.destroy_all
 Event.destroy_all
+Schedule.destroy_all
+User.destroy_all
+Project.destroy_all
 
 puts "Creating Users..."
 
