1d49ac404af4f317928f069666868f2c22f4e6a4
Kali Deming <kalideming@icloud.com>
2023-02-07 10:48:41 -0700

    company controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,13 @@
 class CompaniesController < ApplicationController
+
+    def index 
+        companies = Company.all 
+        render json: projects, status: :ok 
+    end
+
+    def show 
+        company = Company.find(params[:id])
+        render json: company, status: :ok 
+    end
+
 end
